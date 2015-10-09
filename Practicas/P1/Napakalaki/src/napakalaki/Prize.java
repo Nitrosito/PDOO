4c05d934638dbf8c4598813d6f64082f1f8c5f17
Nitrosito <nitrosito@gmail.com>
2015-10-09 11:56:16 +0200

    Añadida clase BadConsequence,Prize y constructores

---
+++ Practicas/P1/Napakalaki/src/napakalaki/Prize.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author nitrosito
+ */
+public class Prize {
+    private int treasures;
+    private int level;
+    
+    
+    public Prize(int treasures, int level){
+            this.treasures=treasures;
+            this.level=level;
+    }
+    
+    public int getTreasures(){
+            return treasures;
+    }
+    
+    public int getLevel(){
+            return level;
+    }
+    
+}
