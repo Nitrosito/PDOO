a3438fecee31a099fcf4a61f692dc1d22888d080
Nitrosito <nitrosito@gmail.com>
2015-10-16 21:01:21 +0200

    Creado arrayde monstruos, añadido monstruo ejemplo

--- Practicas/P1/Napakalaki/src/napakalaki/PruebaNapakalaki.java
+++ Practicas/P1/Napakalaki/src/napakalaki/PruebaNapakalaki.java
@@ -17,13 +17,22 @@ public class PruebaNapakalaki {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+       /* PROBADO DE METODOS DE CREACIÓN Y TOSTRING
             Prize miPrize = new Prize(2,3); // Creo premio        
             BadConsequence mibc = new BadConsequence("Muerte subita",3,0,0);// Creo mala consecuencia
             
             Monster Mimonstruo = new Monster("Atún",10,mibc,miPrize);// Creo Monstruo
-            //System.out.println(miPrize.getTreasures());
-            //System.out.println(miPrize.getLevel());
+            System.out.println(miPrize.getTreasures());
+            System.out.println(miPrize.getLevel());
             System.out.print(Mimonstruo.toString()); // Testeando todos los tostring
+        */
+        
+            ArrayList<Monster> monstruos = new ArrayList(); // Array de monstruos
+            
+            BadConsequence badConsequence = new BadConsequence("Pierdes 5 niveles y 3 tesoros visibles",5,3,0);
+            Prize prize = new Prize(4,2);
+            monstruos.add(new Monster("El rey de la rosa",13,badConsequence,prize));
     }
     
 }
