d3150b8c837575aaf2193ff2820992f1984ce42a
Nitrosito <nitrosito@gmail.com>
2015-10-16 21:16:22 +0200

    Añadido monstruo angeles de la noche ibicenca

--- Practicas/P1/Napakalaki/src/napakalaki/PruebaNapakalaki.java
+++ Practicas/P1/Napakalaki/src/napakalaki/PruebaNapakalaki.java
@@ -6,6 +6,7 @@
 package napakalaki;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
@@ -30,9 +31,22 @@ public class PruebaNapakalaki {
         
             ArrayList<Monster> monstruos = new ArrayList(); // Array de monstruos
             
+            // AÑADIENDO CARTAS
+            
+                // El rey de la rosa
             BadConsequence badConsequence = new BadConsequence("Pierdes 5 niveles y 3 tesoros visibles",5,3,0);
             Prize prize = new Prize(4,2);
             monstruos.add(new Monster("El rey de la rosa",13,badConsequence,prize));
+            
+                // Angeles de la noche ibicenca
+            badConsequence = new BadConsequence("Te atrapan para llevarte de fiesta y te dejan"
+                    + "caer en mitad del vuelo. Descarta 1 mano visible y 1 mano oculta",0,
+                    new ArrayList(Arrays.asList(TreasureKind.ONEHAND)),
+                    new ArrayList(Arrays.asList(TreasureKind.ONEHAND)));
+            
+                    prize = new Prize(4,1);
+                    monstruos.add(new Monster("Angeles de la noche ibicenca",14,badConsequence,prize));
+                            
     }
     
 }
