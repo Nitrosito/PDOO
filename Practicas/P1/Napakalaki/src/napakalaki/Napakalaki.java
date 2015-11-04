8d07c4472d38c59b3ceef210a011b4f241237cb7
Nitrosito <nitrosito@gmail.com>
2015-11-04 21:43:06 +0100

    Acabado introducir metodos Napakalaki

---
+++ Practicas/P1/Napakalaki/src/napakalaki/Napakalaki.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nitrosito
+ */
+public class Napakalaki {
+    
+    private Monster currentMonster;
+    private CardDealer dealer;
+    private Player currentPlayer;
+    private Player players;
+    //Y combatResult?¿?
+    private enum CombatResult; //??
+        
+
+    
+    //Private
+    private static final Napakalaki instance = new Napakalaki();
+    
+    private Napakalaki(){
+     // Aqui va la asignación de valores, etc
+    }
+
+    private static Napakalaki getInstance(){
+        return instance;
+    }
+    
+    private void initPlayers(ArrayList<String> nombres){
+       ///
+    }
+    
+    private Player nextPlayer(){
+        ///
+    }
+    
+    private boolean nextTurnAllowed(){
+        //
+    }
+    
+    private void setEnemies(){
+        //
+    }
+    
+    public Napakalaki getInstance(){ // #FIXME Preguntar por que subrayado
+        //
+    }
+    
+    public CombatResult developCombat(){
+        //
+    }
+    
+    public void discardVisibleTreasures(Treasure treasures){
+        //
+    }
+    
+    public void discardHiddenTreasures(Treasures treasures){
+        //
+    }
+    
+    public void makeTreasuresVisible(Treasures treasures){
+        //
+    }
+    
+    public void initGame(string players){
+        //
+    }
+    
+    public Player getCurrentPlayers(){
+        //
+    }
+    
+    public Monster getCurrentMonster(){
+        //
+    }
+    
+    public boolean nextTurn(){
+        //
+    }
+    
+    public boolean endOfGame(CombatResult result){
+        //
+    }
+    
+    
+    
+    
+        
+//Public
+    
+}
