b4ae7e1abecb6958a3cbea914f0c891495b2bc73
Nitrosito <nitrosito@gmail.com>
2015-11-05 22:19:30 +0100

    Acabado Dice

---
+++ Practicas/P1/Napakalaki/src/napakalaki/Dice.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author nitrosito
+ */
+public class Dice {
+    
+     private static final Dice instance = new Dice();
+     
+      private Dice(){
+     // Aqui va la asignación de valores, etc
+    }
+
+    private static Dice getInstance(){
+        return instance;
+    }
+    
+    public Dice getInstance(){
+        //
+    }
+    
+    public int nextNumber(){
+        //
+    }
+    
+}
