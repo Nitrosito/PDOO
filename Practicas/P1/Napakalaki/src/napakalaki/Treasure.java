eacc6b4ad3ee36798424fc67ed7f95456f584bf4
Nitrosito <nitrosito@gmail.com>
2015-11-05 22:16:15 +0100

    AÑadidos metodos treasure

---
+++ Practicas/P1/Napakalaki/src/napakalaki/Treasure.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author nitrosito
+ */
+public class Treasure {
+    private String name;
+    private int bonus;
+    private TreasureKind type;
+    
+    public Treasure(string n,int bonus,TresureKind t){
+        //
+    }
+    
+    public String getName(){
+        //
+    }
+    
+    public int getBonus(){
+        //
+    }
+    
+    public TreasureKind getType(){
+        //
+    }
+    
+    
+}
