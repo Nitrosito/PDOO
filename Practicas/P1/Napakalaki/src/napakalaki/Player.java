c46737d6c9e49643b319ac539dd5e10281c2fe28
Nitrosito <nitrosito@gmail.com>
2015-11-10 10:30:59 +0100

    Manual Merge with nicojava

--- Practicas/P1/Napakalaki/src/napakalaki/Player.java
+++ Practicas/P1/Napakalaki/src/napakalaki/Player.java
@@ -16,8 +16,8 @@ public class Player {
    private boolean dead=true;
    private boolean canlSteal=true;
    private Player enemy;
-   private Treasure hiddenTreasures;
-   private Treasure visibleTreasures;
+   private ArrayList<Treasure> hiddenTreasures;
+   private ArrayList<Treasure> visibleTreasures;
    private BadConsequence pendingBadConsequence;
    
    static final int MAXLEVEL=10;
@@ -30,19 +30,37 @@ public class Player {
    }
    
    private void bringToLife(){
+       dead=true;
    }
    
    private int getCombatLevel(){
-       return level;
+       int combatLevel;
+       int bonus=0;
+       
+       for (Treasure visibleTreasure : visibleTreasures) {
+           bonus = bonus + visibleTreasure.getBonus();
+       }
+       
+       combatLevel=level+bonus;
+       return combatLevel;
    }
    
    private void incrementLevels(int i){
+       level=level+i;
+       if(level>10){
+           level=10;
+       }
    }
    
    private void decrementLevels(int i){
+       level=level-i;
+       if(level<1){
+           level=1;
+       }
    }
    
    private void setPendingBadConsequence(BadConsequence b){
+       pendingBadConsequence=b;
    }
    
    private void applyPrize(Monster m){
@@ -58,9 +76,13 @@ public class Player {
    }
    
    private void dielNoTreasures(){
+       if(visibleTreasures.size()==0 && hiddenTreasures.size()==0){
+           dead=false;
+       }
    }
    
    public boolean isDead(){
+       return dead;
    }
    
    public Treasure[] getHiddenTreasure(){
@@ -82,6 +104,10 @@ public class Player {
    }
    
    public boolean validState(){
+       if(hiddenTreasures.size()<=4 && pendingBadConsequence.isEmpy()){
+           return true;
+       }
+       return false;
    }
    
    public void initTreasure(){
@@ -95,6 +121,7 @@ public class Player {
    }
    
    public void setEnemy(Player enemy){
+       this.enemy=enemy;
    }
    
    private Treasure giveMeATreasure(){
