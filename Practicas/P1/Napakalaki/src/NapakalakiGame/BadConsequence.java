a23675bf1bbec2bc5293679059d3f88834e27ad7
Nitrosito <nitrosito@gmail.com>
2016-01-05 12:07:11 +0100

    p4 1 carlos

--- Practicas/P1/Napakalaki/src/NapakalakiGame/BadConsequence.java
+++ Practicas/P1/Napakalaki/src/NapakalakiGame/BadConsequence.java
@@ -10,169 +10,121 @@ import java.util.ArrayList;
  *
  * @author nitrosito
  */
-public class BadConsequence {
-    private ArrayList<TreasureKind> specificHiddenTreasures= new ArrayList();
-    private ArrayList<TreasureKind> specificVisibleTreasures= new ArrayList();
-
-    private String text;
-    private int levels;
-    private int nVisibleTreasures;
-    private int nHiddenTreasures;
-    private boolean death;
+public abstract class BadConsequence {
+    String text;
+    int levels;
     static final int MAXTREASURES =10;
     
-    public boolean isEmpty(){
-        if(nVisibleTreasures==0 && nHiddenTreasures==0 &&
-           specificHiddenTreasures.size()==0 && specificVisibleTreasures.size()==0){
-            return true;
-        }
-        return false;
+    public BadConsequence(String text, int levels){
+        this.text=text;
+        this.levels=levels;
     }
+    
+    public abstract boolean isEmpty();
     
     
     public int getLevels(){
             return levels;
     }
+      
+    abstract void substractVisibleTreasure(Treasure t);
+//    {
+//         boolean esta = false;
+//        int ind;
+//        for(ind = 0; ind < specificVisibleTreasures.size() && esta!=true; ind++){
+//            if(t.getType() == specificVisibleTreasures.get(ind))
+//                esta = true;
+//        }
+//        if(esta)
+//           specificVisibleTreasures.remove(ind);
+//        else if (nVisibleTreasures > 0)
+//            nVisibleTreasures -= 1;
+//    }
     
-    
-    public int getnVisibleTreasures(){
-        return nVisibleTreasures;
-    }
-    
-    
-    public int getnHiddenTreasures(){
-        return nHiddenTreasures;
-    }
-    
-    public ArrayList<TreasureKind> getSpecificVisibleTreasures(){
-        return specificVisibleTreasures;
-    }
-    
-    
-    public ArrayList<TreasureKind> getSpecificHiddenTreasures(){
-        return specificHiddenTreasures;
-    }
-    
-    void substractVisibleTreasure(Treasure t){
-         boolean esta = false;
-        int ind;
-        for(ind = 0; ind < specificVisibleTreasures.size() && esta!=true; ind++){
-            if(t.getType() == specificVisibleTreasures.get(ind))
-                esta = true;
-        }
-        if(esta)
-           specificVisibleTreasures.remove(ind);
-        else if (nVisibleTreasures > 0)
-            nVisibleTreasures -= 1;
-    }
-    
-    void substractHiddenTreasure(Treasure t){
-                 boolean esta = false;
-        int ind;
-        for(ind = 0; ind < specificHiddenTreasures.size() && esta!=true ; ind++){
-            if(t.getType() == specificHiddenTreasures.get(ind))
-                esta = true;
-        }
-        if(esta)
-           specificHiddenTreasures.remove(ind);
-        else if (nHiddenTreasures > 0)
-            nHiddenTreasures -= 1;
-    }
-    
-    
+    abstract void substractHiddenTreasure(Treasure t);
+//    {
+//                 boolean esta = false;
+//        int ind;
+//        for(ind = 0; ind < specificHiddenTreasures.size() && esta!=true ; ind++){
+//            if(t.getType() == specificHiddenTreasures.get(ind))
+//                esta = true;
+//        }
+//        if(esta)
+//           specificHiddenTreasures.remove(ind);
+//        else if (nHiddenTreasures > 0)
+//            nHiddenTreasures -= 1;
+//    }
+//    
+//    
     public String getText(){
             return text;
     }
 
-    
+     
 
+//    public BadConsequence(String text,boolean death){
+//        this.text="";
+//        this.levels=Player.MAXLEVEL;
+//        this.nVisibleTreasures=MAXTREASURES;
+//        this.nHiddenTreasures=MAXTREASURES;
+//        this.text=text;
+//        this.death=death;
+//    }
     
-    public boolean getDeath(){
-        return death;
-    }
+//        public BadConsequence(String text, int levels, ArrayList<TreasureKind> tVisible, ArrayList<TreasureKind> tHidden) {
+//        this.text=text;
+//        this.levels=levels;
+//        this.nVisibleTreasures=0;
+//        this.nHiddenTreasures=0;
+//        this.specificVisibleTreasures=tVisible;
+//        this.specificHiddenTreasures=tHidden;
+//        this.death=false;
+//    }
+//        
+public abstract BadConsequence adjustToFitTreasureLists(ArrayList<Treasure> v, ArrayList<Treasure> h);   
+//    int visibles=0;
+//    int ocultos=0;
+//    ArrayList<TreasureKind> espVisibles = new ArrayList();
+//    ArrayList<TreasureKind> espOcultos = new ArrayList();
+//
+//        if(nVisibleTreasures > 0){ //o te quita visibles
+//            if(nVisibleTreasures > v.size()){
+//                visibles = v.size();
+//            }
+//            else{
+//                visibles = nVisibleTreasures;
+//            }
+//        }
+//        else{ // o especificos
+//            for (TreasureKind specificVisibleTreasure : specificVisibleTreasures) {
+//                for(Treasure treasure : v){
+//                    if(treasure.getType() == specificVisibleTreasure)
+//                        espVisibles.add(specificVisibleTreasure);
+//                }                 
+//            }
+//        }
+//        if(nHiddenTreasures > 0){
+//            if(nHiddenTreasures > h.size()){
+//                ocultos = h.size();
+//            }
+//            else{
+//                ocultos = nHiddenTreasures;
+//            }
+//        }
+//        else{
+//           for (TreasureKind specificHiddenTreasure : specificHiddenTreasures) {
+//                for(Treasure treasure : h){
+//                    if(treasure.getType() == specificHiddenTreasure)
+//                        espOcultos.add(specificHiddenTreasure);
+//                }                 
+//            }
+//        }
+//       BadConsequence res = new BadConsequence("", 0, espVisibles, espOcultos);
+//       res.nHiddenTreasures=ocultos;
+//       res.nVisibleTreasures=visibles;
+//       return res;
+//        
+//    }
     
-        
-    
-    public BadConsequence(String text, int levels, int nVisible, int nHidden){
-            this.text=text;
-            this.levels=levels;
-            this.nVisibleTreasures=nVisible;
-            this.nHiddenTreasures=nHidden;
-            this.death=false;
-    }
-    
-    public BadConsequence(String text,boolean death){
-        this.text="";
-        this.levels=Player.MAXLEVEL;
-        this.nVisibleTreasures=MAXTREASURES;
-        this.nHiddenTreasures=MAXTREASURES;
-        this.text=text;
-        this.death=death;
-    }
-    
-        public BadConsequence(String text, int levels, ArrayList<TreasureKind> tVisible, ArrayList<TreasureKind> tHidden) {
-        this.text=text;
-        this.levels=levels;
-        this.nVisibleTreasures=0;
-        this.nHiddenTreasures=0;
-        this.specificVisibleTreasures=tVisible;
-        this.specificHiddenTreasures=tHidden;
-        this.death=false;
-    }
-        
-public BadConsequence adjustToFitTreasureLists(ArrayList<Treasure> v, ArrayList<Treasure> h){   
-    int visibles=0;
-    int ocultos=0;
-    ArrayList<TreasureKind> espVisibles = new ArrayList();
-    ArrayList<TreasureKind> espOcultos = new ArrayList();
-
-        if(nVisibleTreasures > 0){ //o te quita visibles
-            if(nVisibleTreasures > v.size()){
-                visibles = v.size();
-            }
-            else{
-                visibles = nVisibleTreasures;
-            }
-        }
-        else{ // o especificos
-            for (TreasureKind specificVisibleTreasure : specificVisibleTreasures) {
-                for(Treasure treasure : v){
-                    if(treasure.getType() == specificVisibleTreasure)
-                        espVisibles.add(specificVisibleTreasure);
-                }                 
-            }
-        }
-        if(nHiddenTreasures > 0){
-            if(nHiddenTreasures > h.size()){
-                ocultos = h.size();
-            }
-            else{
-                ocultos = nHiddenTreasures;
-            }
-        }
-        else{
-           for (TreasureKind specificHiddenTreasure : specificHiddenTreasures) {
-                for(Treasure treasure : h){
-                    if(treasure.getType() == specificHiddenTreasure)
-                        espOcultos.add(specificHiddenTreasure);
-                }                 
-            }
-        }
-       BadConsequence res = new BadConsequence("", 0, espVisibles, espOcultos);
-       res.nHiddenTreasures=ocultos;
-       res.nVisibleTreasures=visibles;
-       return res;
-        
-    }
-    
-    
-    public String toString(){
-        return "\n Mala Consecuencia \n       Nombre mala consecuencia:" + this.text + "\n       Niveles:" + Integer.toString(this.levels) 
-                + "\n       Tesoros Visibles:" + Integer.toString(this.nVisibleTreasures) +
-                "\n       Tesoros No Visibles:" + Integer.toHexString(this.nHiddenTreasures) +
-                "\n       Muerte:" + this.death;
-    }
-    
-    
-        
+       
 }
---
+++ Practicas/P1/Napakalaki/src/NapakalakiGame/NumericBC.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2016 nitrosito
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package NapakalakiGame;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nitrosito
+ */
+public class NumericBC extends BadConsequence{
+    private int nVisibleTreasures;
+    private int nHiddenTreasures;
+    
+ public NumericBC(String text, int levels, int nVisible, int nHidden){
+            super(text,levels);
+            this.nVisibleTreasures=nVisible;
+            this.nHiddenTreasures=nHidden;
+}
+ 
+public int getnVisibleTreasures(){
+    return nVisibleTreasures;
+}
+
+public int getnHiddenTreasures(){
+    return nHiddenTreasures;
+}
+
+@Override
+public boolean isEmpty(){
+    if(nVisibleTreasures==0 && nHiddenTreasures==0)
+        return true;
+    
+    return false;
+}
+
+@Override
+void substractVisibleTreasure(Treasure t){
+        if(nVisibleTreasures>0)
+            nVisibleTreasures--;
+}
+
+@Override
+void substractHiddenTreasure(Treasure t){
+        if(nHiddenTreasures>0)
+            nHiddenTreasures--;
+}
+
+@Override
+public NumericBC adjustToFitTreasureLists(ArrayList<Treasure> v, ArrayList<Treasure> h){   
+    int visibles=0;
+    int ocultos=0;
+
+        if(nVisibleTreasures > 0){ //o te quita visibles
+            if(nVisibleTreasures > v.size()){
+                visibles = v.size();
+            }
+            else{
+                visibles = nVisibleTreasures;
+            }
+        }
+
+        if(nHiddenTreasures > 0){
+            if(nHiddenTreasures > h.size()){
+                ocultos = h.size();
+            }
+            else{
+                ocultos = nHiddenTreasures;
+            }
+        }
+
+       NumericBC res = new NumericBC("", 0, visibles, ocultos);
+       res.nHiddenTreasures=ocultos;
+       res.nVisibleTreasures=visibles;
+       return res;
+        
+    }
+
+    public String toString(){
+        return "\n Mala Consecuencia \n       Nombre mala consecuencia:" + this.text + "\n       Niveles:" + Integer.toString(this.levels) 
+                + "\n       Tesoros Visibles:" + Integer.toString(this.nVisibleTreasures) +
+                "\n       Tesoros No Visibles:" + Integer.toHexString(this.nHiddenTreasures);
+                
+    }
+    
+
+
+
+}
